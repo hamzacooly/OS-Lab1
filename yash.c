fda51e7dfa0724d700852e709d51b41856d0458c
Hamza Khatri <hamzakhatri@utexas.edu>
2017-09-19 15:04:18 -0500

    Added some more shtuff.

--- yash.c
+++ yash.c
@@ -10,28 +10,155 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <string.h>
 
 int pipefd[2];
 int pid_ch1, pid_ch2, pid;
 
-static void sig_int(int signo) {
-    printf("Sending signals to group:%d\n",pid_ch1); // group id is pid of first in pipeline
-    kill(-pid_ch1,SIGINT);
-}
-static void sig_tstp(int signo) {
-    printf("Sending SIGTSTP to group:%d\n",pid_ch1); // group id is pid of first in pipeline
-    kill(-pid_ch1,SIGTSTP);
+typedef struct process *Process;
+typedef struct job *Job;
+
+struct process {
+    struct process *next;
+    pid_t pid;                  /* process id                       */
+    char text[2000];	/* text to print when 'jobs' is run */
+    int status;			/* return code from waitpid */
+};
+
+struct job {
+    pid_t gleader;		/* process group leader of this job  */
+    pid_t other;		/* subjob id (SUPERJOB)
+				 * or subshell pid (SUBJOB) */
+    int stat;                   /* see STATs below                   */
+    char *pwd;			/* current working dir of shell when *
+				 * this job was spawned              */
+    struct process *procs;	/* list of processes                 */
+    struct process *auxprocs;	/* auxiliary processes e.g multios   */
+    LinkList filelist;		/* list of files to delete when done */
+				/* elements are struct jobfile */
+    int stty_in_env;		/* if STTY=... is present            */
+    struct ttyinfo *ty;		/* the modes specified by STTY       */
+};
+
+// static void sig_int(int signo) {
+//     printf("Sending signals to group:%d\n",pid_ch1); // group id is pid of first in pipeline
+//     kill(-pid_ch1,SIGINT);
+// }
+// static void sig_tstp(int signo) {
+//     printf("Sending SIGTSTP to group:%d\n",pid_ch1); // group id is pid of first in pipeline
+//     kill(-pid_ch1,SIGTSTP);
+// }
+
+void builtins(char* cmd){
+    if(strcmp(cmd, "fg") == 0){
+
+    }
+    else if(strcmp(cmd, "bg") == 0){
+
+    }
+    else if(strcmp(cmd, "jobs") == 0){
+
+    }
 }
 
-int main (void) {
-    // Buffer for a max of 2k chars
+void eval_cmds(int len, int maxlen, char* arr[len][maxlen]){
+    int i;
+    for(i = 0; i < len; i++){
+        
+    }
+}
+
+void parse_stuff(int len, char* arr[]){
+    int i, j=1, k=0;
+    // Counts how much mem needed to allocate
+    for(i = 0; i < len; i++){
+        if(arr[i] == NULL)
+            break;
+        if(strcmp(arr[i], "<") == 0){
+            j++;
+        }
+        else if(strcmp(arr[i], ">") == 0){
+            j++;
+        }
+        else if(strcmp(arr[i], "2>") == 0){
+            j++;
+        }
+        else if(strcmp(arr[i], "|") == 0){
+            j++;
+        }
+        else if(strcmp(arr[i], "&") == 0){
+            j++;
+        }
+    }
+    char* cmds[j + j][len + 1];
+    int cmds_len = j + j;
+    j = 0;
+    for(i = 0; i < len; i++){
+        if(arr[i] == NULL){
+            cmds[j][k] = NULL;
+            break;
+        }
+        if(strcmp(arr[i], "<") == 0){
+            cmds[j][k] = NULL;
+            j++;
+            cmds[j][0] = arr[i];
+            cmds[j][1] = NULL;
+            j++;
+            k = 0;
+        }
+        else if(strcmp(arr[i], ">") == 0){
+            cmds[j][k] = NULL;
+            j++;
+            cmds[j][0] = arr[i];
+            cmds[j][1] = NULL;
+            j++;
+            k = 0;
+        }
+        else if(strcmp(arr[i], "2>") == 0){
+            cmds[j][k] = NULL;
+            j++;
+            cmds[j][0] = arr[i];
+            cmds[j][1] = NULL;
+            j++;
+            k = 0;
+        }
+        else if(strcmp(arr[i], "|") == 0){
+            cmds[j][k] = NULL;
+            j++;
+            cmds[j][0] = arr[i];
+            cmds[j][1] = NULL;
+            j++;
+            k = 0;
+        }
+        else if(strcmp(arr[i], "&") == 0){
+            cmds[j][k] = NULL;
+            j++;
+            cmds[j][0] = arr[i];
+            cmds[j][1] = NULL;
+            j++;
+            k = 0;
+        }
+        else{
+            cmds[j][k] = arr[i];
+            k++;
+        }
+    }
+    free(arr);
+    eval_cmds(cmds_len, len+1, cmds);
+}
+
+void prompt(void){
     char buf[2000];
-    int status;
-    while(1){
-        // Read in the input and tokenize the string
-        printf("# ");
-        fgets(buf, 2000, stdin);
+    printf("# ");
+    fgets(buf, 2000, stdin);
+    if(strcmp(buf, "fg") == 0 
+        || strcmp(buf, "bg") == 0
+        || strcmp(buf, "jobs") == 0){
+        builtins(buf);
+    }
+    else{
         char* token = strtok(buf, " ");
         char** tokenz = NULL;
         int num_spaces = 0;
@@ -43,33 +170,18 @@ int main (void) {
             if(tokenz == NULL)
                 exit(-1); //mem alloc failed.
             tokenz[num_spaces-1] = token;
-            token = strtok(NULL, " ");
+            token = strtok(NULL, "  ");
         }
         tokenz = realloc(tokenz, sizeof(char*) * ++num_spaces);
         tokenz[num_spaces-1] = NULL;
-        int tokenz_len = num_spaces;
+        int tokenz_len = num_spaces; // Size of tokenz array
+        parse_stuff(tokenz_len, tokenz);
+    }
+}
 
-        pid_t cpid = fork();
-        if(cpid == 0){
-            // i is the current pos.
-            // j is the last start pos.
-            int i = 0, j = 0;
-            char* special = "<>2>|";
-            while((token = tokenz[i]) != NULL){
-                if(strstr(special, token)){
-                    int k = j;
-                    int s = i - j + 1;
-                    for(k; k < s; k++){
-                        
-                    }
-                }
-            }
-            execvp(tokenz[0], tokenz);
-            free(tokenz);
-        }
-        else{
-            wait(NULL);
-        }
-        // DO STUFF HERE
+int main (void) {
+    int status;
+    while(1){
+        prompt();
     }
 }
